135f057f086f60e5f592b6a96dd7e7f5b09a2443
leigq <191789784@qq.com>
2019-04-24 15:53:11 +0800

    修改配置

--- server/src/main/java/com/leigq/www/server/controller/OrderMasterController.java
+++ server/src/main/java/com/leigq/www/server/controller/OrderMasterController.java
@@ -26,6 +26,8 @@ public class OrderMasterController {
 
     private final Response response;
 
+
+    // timeout 可以不指定，但是version一定要指定 不然会找不到服务 直连需要加url="dubbo://localhost:20880"
     @Reference(version = "1.0")
     private CommodityService commodityService;
 
